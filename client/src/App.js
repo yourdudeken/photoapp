e30bf2a62003bffa80da77c68169a5dfdec21a89
yourdudeken <kenmwendwamuthengi@gmail.com>
2024-06-08 00:02:22 +0300

    initialcommit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+// client/src/App.js
+import React from 'react';
+import ImageUpload from './components/ImageUpload';
+import ImageList from './components/ImageList';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Image Upload App</h1>
+            <ImageUpload />
+            <ImageList />
+        </div>
+    );
+};
+
+export default App;
